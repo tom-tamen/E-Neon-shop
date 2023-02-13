a42496d3afe890992f38e3e34cf13cddbb44eb74
Tom Tamen <tomtamen@gmx.fr>
2023-02-13 22:28:24 +0100

    :card_file_box: fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,48 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user->setEmail('u' . $i . '@mail.com');
+            $user->setPassword($this->passwordHasher->hashPassword($user, '123'));
+            $user->setRoles(['ROLE_USER']);
+            if($i === 0) $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
 
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $category = new Category();
+            $category->setName('Category ' . $i);
+
+            for($j = 0; $j < 20; $j++) {
+                $product = new Product();
+                $product->setName('Product ' . $j);
+                $product->setPrice(rand(1, 500)+(rand(0, 10) / 10));
+                $product->setStock(rand(10, 100));
+                $product->setCategory($category);
+                $product->setDescription('Product ' . $j .' from category '. $i .' Cupcake ipsum dolor sit amet candy canes icing candy canes tart. Brownie cupcake I love tart sugar plum. Caramels carrot cake jelly-o gingerbread sweet chocolate cake I love biscuit macaroon.');
+                $manager->persist($product);
+            }
+
+            $manager->persist($category);
+        }
         $manager->flush();
     }
 }
